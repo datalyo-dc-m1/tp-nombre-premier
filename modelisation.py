69ed43d2874aa05c4e8f1faeddd939a17b76a703
arthuragn <arthur270a@gmail.com>
2020-04-22 11:19:08 +0200

    Finalisation de la class Number, et de ses méthodes

--- modelisation.py
+++ modelisation.py
@@ -1,12 +1,23 @@
 class Number:
-    def __init__(self,number):
-        self.number = number
-        number > 0
+    def __init__(self,number=1):
+        self.number = int(number)
 
-    def is_divisible(self,i):
-        self.i=i
+    def is_divisible(self,i=1):
+        return self.number % i == 0
 
     def is_prime(self):
+        num = self.number
+        j = True
+        if num % 2 == 0 and num != 2:
+            j = False
+        else:
+            if num > 1:
+                for i in range(2, num // 2 + 1):
+                    if num % i == 0:
+                        j = False
+            else:
+                j = False
+        return j
 
 
 
